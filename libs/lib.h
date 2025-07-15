af4977daf941e401ad2603f52d406f46a5dc9d34
Camilla Matos <camilla@debian.camilla>
2025-07-15 10:20:55 -0300

    adicionando protoripos

--- libs/lib.h
+++ libs/lib.h
@@ -5,6 +5,7 @@ typedef struct MateriaPrima{
   char *nome;
   float preco;
   int codigo;
+  int altura; // altura do nó para balanceamentos 
   struct MateriaPrima *esq, *dir;
 } MatPrima;
 
@@ -31,24 +32,30 @@ void lerCSV(FILE *arquivo); // lê um arq csv e escreve-o em lista ou arvore
 int fecharArquivo(FILE *arquivo); // fecha arquivo
 
 // implementação e manipulação de lista duplamente encadeada
-// criar no, inserir no inicio/fim, percorrer, excluir...
+// mat produto
+// produto
 
-// implementação e manipulação de arvore AVL
-// inserções, rotações, percurso, excluir...
-
-// funçoes para a lista de produtos, incluem produtos.csv e dll.c
-// inserir, excluir e alterar, salvar modificações no arquivo, listar todos, consultar por nome/codigo   
-
-// funções para a lista de matéria prima, incluem matPrima.csv e tree.c
-// inserir, excluir e alterar, salvar modificações no arquivo, listar todos, consultar por nome/codigo   
+// arvore avl para manipulaçaõ de matéria prima 
+MatPrima *novoNo(char *nome, float preco); // obs* novo->codigo = gerarCodigo(char *str)
+MatPrima *inserirArvore(MatPrima *no, char *nome, float preco);
+MatPrima *rotacaoDir(MatPrima *no);
+MatPrima *rotacaoEsq(MatPrima *no);
+MatPrima *searchFromTree(MatPrima *head, int codigo);
+MatPrima *removeFromTree(MatPrima *head, int codigo);
+MatPrima *editTreeNode(MatPrima *head, int codigo);
+int obterAltura(MatPrima *no);
+void inOrder(MatPrima *head);
+void freeTree(MatPrima *head);
 
 // utilitários
 void menu(); // UI
 void limparTela();
 int getOpcao(); // lê opcao e trata erros antes de retornar
-void opcaoHandler(int opcao);
+void opcaoHandler(int opcao); // recebe opcaoe direciona para as funcionalidades
 int gerarCodigo(char *str); // recebe uma string e retorna um codigo/hash
 float calcularPrecoVenda(float margemLucro, MatPrima *listaMateriais);
 float calcularPrecoCusto(MatPrima* listaMateriais);
+int max(int a, int b); // Retorna o maior entre dois inteiros               
+
 
 #endif
