2804cd334b65de8c8510ec052737c6a1bbf7004d
Eu, de paula <gfpaula.cic@uesc.br>
2025-07-15 19:44:08 -0300

    reorganizacao5

--- src/main.c
+++ src/main.c
@@ -3,10 +3,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h> 
+#ifdef _WIN32 || _WIN64
+    #include <Windows.h>
+#else
+    #include <unistd.h>
+#endif
 
 int main(int argc, char *argv[])
 {
 
-    
+printf("Hello, World!\n");
+sleep(500);
+printf("funcionou\n");
 return 0;
 }
